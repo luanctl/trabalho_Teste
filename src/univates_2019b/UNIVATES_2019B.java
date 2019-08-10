7e6d63f4ff144f854daef11d9bd1ce1b42f7a07f
Luan <Luan@DESKTOP-2NTN0JR>
2019-08-09 22:02:48 -0300

    Mudança para o trabalho

---
+++ src/univates_2019b/UNIVATES_2019B.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+   autor: Luan Castoldi
+*/
+package univates_2019b;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author luan.castoldi
+ */
+public class UNIVATES_2019B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       
+   
+
+  Pessoa pessoa1 = new Pessoa(); 
+  Pessoa pessoa2 = new Pessoa();
+           
+   //Para Depositar para o Luan é só colocar o numero POSITIVO
+   //Para Depositar para o Renan é só colocar o numero NEGATIVO
+     int valorTransferencia = 300;
+  
+        System.out.println("Saldo incial " + "\n");      
+       pessoa1.setNome("Luan");
+       pessoa1.setValorConta(500);
+       System.out.println(pessoa1);
+       
+       
+        pessoa2.setNome("Renan");
+        pessoa2.setValorConta(500);
+        System.out.println(pessoa2);
+        
+      transferencia(pessoa1.getValorConta(), valorTransferencia);
+
+    }
+           
+        public static void transferencia(double tra1, double tra2) {    
+        double result = tra1 + tra2;
+        double result1 = tra1 - tra2;
+        System.out.println("Valores Atualizados: " + "\n" + "Valor = "+ result + "\n" + "Valor = "+ result1);
+         
+         }
+        
+}
