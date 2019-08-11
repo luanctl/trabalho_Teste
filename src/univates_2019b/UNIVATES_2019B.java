95c064ce666a166821c42ef8adc5861bc6374f12
Luan <Luan@DESKTOP-2NTN0JR>
2019-08-10 21:16:58 -0300

    Utilizando método CASE

--- src/univates_2019b/UNIVATES_2019B.java
+++ src/univates_2019b/UNIVATES_2019B.java
@@ -22,18 +22,17 @@ public class UNIVATES_2019B {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-       
-   
 
   Pessoa pessoa1 = new Pessoa(); 
   Pessoa pessoa2 = new Pessoa();
            
    //Para Depositar para o Luan é só colocar o numero POSITIVO
-   //Para Depositar para o Renan é só colocar o numero NEGATIVO
-     int valorTransferencia = -300;
-  
-        System.out.println("Saldo incial " + "\n");      
+   //Para Depositar para o Renan é só colocar o numero NEGATIVO-
+    pessoa1.setNome("Luan");
+    pessoa2.setNome("Renan");
+    
+       System.out.println("Saldo incial " + "\n"); 
+         
        pessoa1.setNome("Luan");
        pessoa1.setValorConta(500);
        System.out.println(pessoa1);
@@ -42,16 +41,56 @@ public class UNIVATES_2019B {
         pessoa2.setNome("Renan");
         pessoa2.setValorConta(500);
         System.out.println(pessoa2);
+    
+   
+   Scanner tecladoN = new Scanner(System.in);
+   System.out.println("Para qual você deseja tranferir ? :  [1] " + pessoa1.getNome() + " [2] " + pessoa2.getNome());
+   int nome  = tecladoN.nextInt();
+   
+   
+   switch (nome) {
+  case 1:
+    System.out.println(pessoa1.getNome()+ " SELECIONADO");
+     Scanner teclado = new Scanner(System.in);
+       System.out.println("Digite o valor da tranferencia (numero inteiro)");
+        double valorTransferencia = teclado.nextInt();
+        double valor = pessoa1.getValorConta();
+          // transferencia(pessoa1.getNome(), pessoa2.getNome(), pessoa1.getValorConta(), valorTransferencia);
+     
+       if(valorTransferencia > 500){
+                System.out.println("Valor não permitido");
+       }else {
+        String nomeP = pessoa1.getNome();
+        String nomeS = pessoa2.getNome();
+        double valorLuan = valor + valorTransferencia;
+        double valorRenan = valor - valorTransferencia;
         
-      transferencia(pessoa1.getValorConta(), valorTransferencia);
-
+         System.out.println("Valores Atualizados da Tranferencia: R$ " + valorTransferencia + "\n" + "\n" + "Nome = " + nomeP + "\n" + "Valor = "+ valorLuan + "\n" + 
+                                                               "--------------------" + "\n" +
+                                                             "Nome = " + nomeS + "\n" + "Valor = " + valorRenan);
+       }
+    break;
+  case 2:
+  System.out.println(pessoa2.getNome()+ " SELECIONADO");
+     Scanner teclado2 = new Scanner(System.in);
+       System.out.println("Digite o valor da tranferencia (numero inteiro)");
+        double valorTransferencia2 = teclado2.nextInt();
+        double valor2 = pessoa2.getValorConta();
+     
+       if(valorTransferencia2 > 500){
+                System.out.println("Valor não permitido");
+       }else {
+        String nomeP = pessoa1.getNome();
+        String nomeS = pessoa2.getNome();
+        double valorLuan = valor2 - valorTransferencia2;
+        double valorRenan = valor2 + valorTransferencia2;
+        
+         System.out.println("Valores Atualizados da Tranferencia: R$ " + valorTransferencia2 + "\n" + "\n" + "Nome = " + nomeP + "\n" + "Valor = "+ valorLuan + "\n" + 
+                                                               "--------------------" + "\n" +
+                                                             "Nome = " + nomeS + "\n" + "Valor = " + valorRenan);
+        break;
+    
+     }
     }
-           
-        public static void transferencia(double tra1, double tra2) {    
-        double result = tra1 + tra2;
-        double result1 = tra1 - tra2;
-        System.out.println("Valores Atualizados: " + "\n" + "Valor = "+ result + "\n" + "Valor = "+ result1);
-         
-         }
-        
-}
+   } 
+  }
